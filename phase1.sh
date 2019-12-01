943abf65bed039d49c91d8855dd699a1f25414eb
rushah05 <46826537+rushah05@users.noreply.github.com>
2019-11-30 23:14:13 -0600

    Update phase1.sh

--- phase1.sh
+++ phase1.sh
@@ -1,14 +1,4 @@
-#!/bin/bash
-#SBATCH -J caqr
-#SBATCH -o test_caqr.o%j
-#SBATCH -t 00:20:00
-#SBATCH -N 1 -n 8 --constraint=gen10 
-#SBATCH -A pwu
-
-#gcc -O3 -o caqr ca_qr.c -lm
-#./caqr
-
-
+// Steps to run program
 module load OpenMPI/intel/3.1.2
 mpicc -O2 ca_qr.c  -m64 -I${MKLROOT}/include -L${MKLROOT}/lib/intel64 -Wl,--no-as-needed -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl -o caqr
 mpirun caqr
